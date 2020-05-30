b484ae67a24fece86ba9b1c1bf7f73afaea057c7
Victor Esteban Fajardo Rojas <u201612009@upc.edu.pe>
2020-05-29 20:52:23 -0500

    Initial Commit

---
+++ src/main/java/com/acme/pregnancysafe/PregnancysafeApplication.java
@@ -0,0 +1,13 @@
+package com.acme.pregnancysafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PregnancysafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PregnancysafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acme/pregnancysafe/PregnancysafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.pregnancysafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PregnancysafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
