25654122b2c3908e5d11b56b53650ddbc3e3b26b
JavierRozas <43117738+JavierRozas@users.noreply.github.com>
2020-06-06 03:26:28 -0500

    Update MedicalAppointmentResource.java

--- src/main/java/com/acme/pregnancysafe/resource/MedicalAppointmentResource.java
+++ src/main/java/com/acme/pregnancysafe/resource/MedicalAppointmentResource.java
@@ -1,4 +1,52 @@
 package com.acme.pregnancysafe.resource;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "TP_MEDICALAPPOINTMENT")
 public class MedicalAppointmentResource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private Long idObstetrician;
+    
+    private Long idMother;
+    
+    private Date date;
+
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdObstetrician() {
+        return idObstetrician;
+    }
+
+    public void setIdObstetrician(Long idObstetrician) {
+        this.idObstetrician = idObstetrician;
+    }
+
+    public Long getIdMother() {
+        return idMother;
+    }
+
+    public void setIdMother(Long idMother) {
+        this.idMother = idMother;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
